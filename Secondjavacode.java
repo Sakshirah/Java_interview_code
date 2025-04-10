f723eb9f5ba6cc4af988e55d899a8b25d0d432a8
Sakshirah <sakshirah2001@gmail.com>
2025-04-10 17:12:38 +0530

    s

---
+++ Secondjavacode.java
@@ -0,0 +1,27 @@
+package Javacode;
+
+public class Secondjavacode {
+
+	public static void main(String[] args) {
+		        int a = 5;
+		        int b = 10;
+
+		        System.out.println("Before swap:");
+		        System.out.println("a = " + a);
+		        System.out.println("b = " + b);
+
+		        // Swapping using addition and subtraction
+		        a = a + b; // a = 5 + 10 = 15
+		        b = a - b; // b = 15 - 10 = 5
+		        a = a - b; // a = 15 - 5 = 10
+
+		        System.out.println("\nAfter swap:");
+		        System.out.println("a = " + a);
+		        System.out.println("b = " + b);
+		    }
+		}
+
+
+	
+
+
